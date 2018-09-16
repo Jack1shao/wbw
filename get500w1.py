f30ac342458da4083694317cbb29084b840189b4
jack1shao <shaojk@163.com>
2018-09-16 17:17:59 +0800

    11

--- get500w1.py
+++ get500w1.py
@@ -9,7 +9,7 @@ import time
 #获取网页文本
 #
 def geturltext(url):
-	url='http://odds.500.com/fenxi1/ouzhi.php?id=665021&ctype=1&start=0&r=1&style=0&guojia=0&chupan=1'
+	#url='http://odds.500.com/fenxi1/ouzhi.php?id=665021&ctype=1&start=0&r=1&style=0&guojia=0&chupan=1'
 
 	#url='http://odds.500.com/fenxi/ouzhi-664999.shtml'
 	header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'}
@@ -31,70 +31,115 @@ def geturltext(url):
 	
 	return htmlfile
 
-#解析网页文本
-def ansy_500wouzhi0(url0):
-	bcyclist=['Unibet','IBCBET','12BET','SportingBet']
+
+
+def getbsxx(soup):
+	bsxxlist=[]
+	soupdz=soup.find_all('a','hd_name')
+	print(soupdz)
+
+	soupbf=soup.find('p','odds_hd_bf')
+	print(soupbf)
+
+	souptime=soup.find('p','game_time')
+	print(souptime)
+	
+
+	return bsxxlist
+
+#解析网页文本,获取500W欧指的前30个博彩哦你公司
+#由于该网站是通过滚轴；按30个数据加载的，所以要
+def ansy_500wouzhi01(id1):
+	#url0='http://odds.500.com/fenxi1/ouzhi.php?id=665021&ctype=1&start=30&r=1&style=0&guojia=0&chupan=1'
+	id1=665021
+	url0='http://odds.500.com/fenxi1/ouzhi.php?id='+str(id1)+'&ctype=1&start=0&r=1&style=0&guojia=0&chupan=1'
 	ouzhilist=[]
 	soup=BeautifulSoup(geturltext(url0),'lxml')
+	getbsxx(soup)
+
+
+
+	souphtml=soup.find_all(id='nowcnum')
+	#获取欧赔公司数量
+	cnum=re.findall('\d+',souphtml[0].text)
+	a=int(int(cnum[0])/30)#除30向下取整数
+
+	for x in range(a):
+		ouzhilist=ouzhilist+ansy_500wouzhi02(id1,30*(x+1))
+	
+
 	souplist=soup.find_all(id='table_cont')
-	#print(souplist)
-	#souplist=soup.find_all('td')
+	list3=[]
 	for tab in souplist:
-		#print(1,tab)
+		
 		for sps in tab.find_all('span','guojia'):
 				sps.decompose()#删除该哦哦你公司国家信息
-		list3=[]
-		list2=[]
+				
 		list1=re.findall('.*?([\u4E00-\u9FA5a-zA-Z0-9.%()]+)',tab.text.strip())
+		
 		y=0
 		bz=25
 		while y+25<len(list1):
+			list2=[]
 			for x in range(bz):
 				list2.append(list1[y+x])
-				if list1[y+x] in bcyclist:
-					y=y+1
+				if x==24 and x+y+25<=len(list1) and list1[y+x]=='同' and list1[y+x+25]=='客':
+					#删除博彩公司后面的异常数据，
+					list1.pop(x+y+3)
+
 			list3.append(list2)
-			list2=[]
+			
 			y=y+bz
-		print(list3)
-			# for x in range(10):
-			# 	list2.append(itm[x])
-			# 	print(list2)
-		# for tr1 in tab.find_all('tr'):
-		# 	#获取序号
-		# 	tagxh=tr1.find('td','td_one')
-		# 	#xh=re.findall('\d+',tagxh.text)
-		# 	print(tagxh)
-		# 	#ouzhilist.append(xh[0])
-		# 	#tagxh.decompose()
-		# 	#获取菠菜公司
-		# 	tagbc=tr1.find('td','tb_plgs')
-			#bc=re.findall('',tagbc.text)
-			#print(tagbc.text.strip())
-			#tagbc.decompose()
-
-			#for td1 in tab.find_all('td'):
-			#print(len(td1),td1.text)
-
-			#tdx=td1.find_all('td')
-			# td1.decompose()
-			#print(td1)
-			# if len(tdx)==0:
-			# 	ouzhilist.append(td1.text.strip())
-			# 	print(td1.text.strip(),0)
-			# else:
-			# 	for x in tdx:
-			# 		print(x.text)
-			# 		ouzhilist.append(x.text.strip())
-			# td1.decompose()
-			#break
+		
 			
-			
-	
+	ouzhilist=list3+ouzhilist
 	#print(ouzhilist)
-		
-		
 	return ouzhilist
+
+def ansy_500wouzhi02(id1,starts):
+	url0='http://odds.500.com/fenxi1/ouzhi.php?id='+str(id1)+'&ctype=1&start='+str(starts)+'&r=1&style=0&guojia=0&chupan=1'
+	ouzhilist=[]
+	list3=[]
+	soup=BeautifulSoup(geturltext(url0),'lxml')
+	# print(soup)
+	for tab in soup:
+		
+		for sps in tab.find_all('span','guojia'):
+				sps.decompose()#删除该哦哦你公司国家信息
+				
+		list1=re.findall('.*?([\u4E00-\u9FA5a-zA-Z0-9.%()-]+)',tab.text.strip())
+		
+		y=0
+		bz=25
+		while y+25<len(list1):
+			list2=[]
+			for x in range(bz):
+				list2.append(list1[y+x])
+				if x==24 and x+y+25<=len(list1) and list1[y+x]=='同' and list1[y+x+25]=='客':
+					#删除博彩公司后面的异常数据，
+					list1.pop(x+y+3)
+
+			list3.append(list2)
+			
+			y=y+bz
+		#print(list3)
+
+	return list3
+
+def ansy_500wouzhi0(id):
+	id=665021
+	url0='http://odds.500.com/fenxi1/ouzhi.php?id='+str(id)+'&ctype=1&start=0&r=1&style=0&guojia=0&chupan=1'
+	ouzhilist=[]
+	soup=BeautifulSoup(geturltext(url0),'lxml')
+	souphtml=soup.find_all(id='nowcnum')
+	a=re.findall('\d+',souphtml[0].text)
+	print(int(int(a[0])/30))
+	pass
+
+
+
+
+
 
 def souphtml(html1):
 	# soup = BeautifulSoup(open('500w.html'))#打开本地文本
@@ -128,4 +173,4 @@ def selum(url):
 #geturltext('')
 # souphtml('')
 #selum('')
-ansy_500wouzhi0('')
+ansy_500wouzhi01('')
