9fa8acaac72f162d4114c0c9ef9d85e0244031d1
jack1shao <shaojk@163.com>
2020-06-17 23:46:21 +0800

    2020061722

--- stock/stockmd.py
+++ stock/stockmd.py
@@ -1,28 +1,67 @@
 #stockmd.py
 #基础
+import os
+from pandas import read_csv
 from abc import ABCMeta, abstractmethod
 class stock(object):
 	"""docstring for stockclass"""
 	def __init__(self, arg):
 		super(stock, self).__init__()
+		#参数为股票代码
 		self.arg = arg
-		self.df='Non1e'
+		#补全股票代码
+		s_n=''
+		ss=str(arg)[-6:]
+		for i in range(len(ss), 6):
+			s_n+='0'
+
+		self.code=s_n+ss
+		#k线数据
+		self.df=None
+
+		
+	#修饰器函数
 	def decorator(self,component):
 		self.component=component
-
-	def getcode():
-		pass
-
+	#获取股票代码
+	def getcode(self):
+		return self.code
+	#获取股票名称
 	def getname(self):
-		print(self.df+'getname')
+		bacs='basc'
+		df=None
+		csv_path='d:/stock_csv/{}.csv'.format(bacs)
+		files1=csv_path
+		if os.path.exists(files1):
+			with open(files1,'r',encoding='utf-8') as csv_file:
+				df = read_csv(csv_file,index_col=0)#指定0列为index列
+
+		if df.empty:
+			return ''
+		
+		for cod,row in df.iterrows():
+			#要补全6位？？？
+			if str(cod)==self.code:
+				return row[0]
+		print('未找到股票名字')	
+		return ''
 		
 
-	def getsz():
-		pass
+	#市值，根据日线
+	def get_sz(self):
+		code1=self.code
+		df=self.get_k_from_csv(code1,'D')
+		high=df[-1:].high.values.tolist()
+		#print(high)
+		df2=self.get_base_from_db()
+		totals=df2.loc[int(code1)].totals
+		sz=totals*high[0]
+		#print(float('%.2f' % sz),'亿')#小数位数
+		return float('%.2f' % sz)
 
 	def getk(self):
-		print('1111')
-		self.df=self.component.getk()+'111'
+		print('1111getk')
+		self.df=self.component.getk()+self.arg
 		return self.component.getk()+'rrr'
 		
 #装饰类
@@ -51,15 +90,26 @@ class w_kl(Finery):
 		print('w_kl')
 		return 'w_kl'
 
-s=stock('002498')
+s=stock('600609')
+print(s.getname())
+print(s.code)
 m_kl=m_kl()
 w=w_kl()
 #m_kl.decorator(w_kl)
 s.decorator(w)
 
-df=s.getk()
-print(df)
-s.getname()
+s.getk()
+print(s.df)
+print('---1')
+s=stock('600609')
+s.decorator(w)
+
+
+print(s.df)
+print('---2')
+s.getk()
+print(s.df)
+#s.getname()
 		
 #命令
 class commandclass(object):
