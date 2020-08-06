8c95dc8017827e73adb63e52932d339eb9dadfe6
jack1shao <shaojk@163.com>
2020-08-06 23:38:48 +0800

    2020080600002

--- stock/stockmd2.py
+++ stock/stockmd2.py
@@ -714,8 +714,8 @@ def yycl(szb):
 	#注入策略
 	strategy[1] = Context(cl1_rsmd(szb))
 	strategy[2] = Context(cl2_cci_cd(szb))
-	strategy[3] = Context(cl_3_b3_j4(szb))
-	strategy[4] = Context(cl4_ccianddmi(szb))
+	#strategy[3] = Context(cl_3_b3_j4(szb))
+	strategy[3] = Context(cl4_ccianddmi(szb))
 	#strategy[5] = Context(cl5_maidianqian(szb))
 
 	code_order=[]
@@ -750,26 +750,23 @@ def test101(code1):
 	#注入策略
 	res=yycl(szb)
 
-	
-
 	#详细分析
 	co=cciorder(szb)
 	list_bloc=co.cci_qr_blok()
-	
 
 	b=co.cci_dmi()
 	l=len(b)
 	dmi=co.getdmi()
 	iii=0
-	for bloc in list_bloc[-3:]:
+	for bloc in list_bloc[-5:]:
 		iii+=1
 		print(' ******** 第{}区域块 *******'.format(iii),bloc)
 		start=bloc[0]
 		end=bloc[1]
-		print('=====up===')
+		print('  up:')
 		for x in b:
 			print(x[7]) if x[6]>=start and x[6]<=end and x[0]=='up' else 0	
-		print('=====dw===')	
+		print('  dw:')	
 		for x in b:
 			print(x) if x[6]>=start and x[6]<=end and x[0]=='dw' else 0
 			
@@ -910,7 +907,7 @@ if __name__ == '__main__':
 	#get_all_orderresult()
 	#print(fl_ordercsv.__doc__)
 	#fl_ordercsv()
-	test101('002444')
+	#test101('601168')
 	main()
 	#test101('000333')
 	
