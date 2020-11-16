d98db349790940159f3840a995471f30897b0fc8
jack1shao <shaojk@163.com>
2020-11-16 21:18:55 +0800

    20201116001

--- ai_zq.py
+++ ai_zq.py
@@ -1,20 +1,26 @@
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import LinearSVC
 import pandas as pd
 import os
 from pandas.core.frame import DataFrame
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler,StandardScaler,OneHotEncoder
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error, explained_variance_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score, recall_score, accuracy_score
 from sklearn.cluster import AgglomerativeClustering,DBSCAN,KMeans
+from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 from zqfenxi import zqfenxi
 import matplotlib.pyplot as plt
 import mglearn
+from sklearn.pipeline import Pipeline  
 
+import numpy as np
 
 
 from sklearn.externals import joblib
@@ -34,38 +40,69 @@ class gu_getfromdb(object):
 		return df
 '''数据预加工'''
 '''数据预处理'''	
-cp='半球'	
+
 class zq_sjcl:
 	'''数据预加工'''
-	
-	def jg(self):
-		zj=zqfenxi(0)
-		zj.creat_mxk(cp)
-		return 0
+
 	def sjcl(self):
 		'''数据预处理'''
 		gg=gu_getfromdb()
 		
-		files1='e:/football/{}1.csv'.format(cp)
-		df=gg.get_fromfiles(files1)
+		#files1='e:/football/{}1.csv'.format(cp)
+		files1='e:/football/ai_zq_sj.csv'
+		#columns=['idnm','zd','kd','zjq','kjq','sg','jp','js1','cp','cs3','bcgs',
+		#'cz3','cz1','cz0','chf_Bet365','jhfc_Bet365','ck3c_Bet365','ck1c_Bet365','ck0c_Bet365',
+		#'chf_Iceland','jhfc_Iceland','ck3c_Iceland','ck1c_Iceland','ck0c_Iceland','lrzs1','ykzs1','lrzs2','ykzs2','lrzs3','ykzs3','gm','fx']
+		#columns=['idnm','zd','kd','zjq','kjq','sg','cp','cz3','cz1','cz0','chf_Bet365','jhfc_Bet365','ck3c_Bet365','ck1c_Bet365','ck0c_Bet365','jhfc_Iceland','ykzs1','ykzs2','ykzs3','fx']
+		columns=['idnm','zd','kd','zjq','kjq','sg','ykzs1','ykzs2','ykzs3','fx','gm','chf_Bet365','jhfc_Bet365']
 
+		df=gg.get_fromfiles(files1)
+		#df=df[df.gm!=0][columns]
+		df=df[columns]
 		ccc=df.columns.values.tolist()
+
 
 		vvv=df.values.tolist()
 		for row in vvv:
-			if row[3]==2:row[3]=3
-
-			row[3]=1 if row[3] in [0,1] else 0
-	
-
+			#if row[3]==2:row[3]=3
+			#row[5]=0 if row[5] in [1,0] else 1#二分类
+			row[10]=row[6]
+			row[9]=row[8]
 			for i in range(0,len(row)):
 				if row[i] is '':
 					row[i]=0
-			#print(row)
+				#	'本场比赛必发成交量倾向于主胜与百家欧赔概率相差不大'
+				if row[i]=='本场比赛必发成交量倾向于主胜与百家欧赔概率相差不大':
+					row[i]='x3cbdf'
+				if row[i]=='本场比赛必发成交量倾向于主胜与百家欧赔概率相差较大，谨防主胜过热':
+					row[i]='x3cdf3r'
+				#本场比赛必发成交量倾向于平局与百家欧赔概率相差较大，谨防平局过热	
+				if row[i]=='本场比赛必发成交量倾向于平局与百家欧赔概率相差不大':
+					row[i]='x1cbdf'
+				if row[i]=='本场比赛必发成交量倾向于平局与百家欧赔概率相差较大，谨防平局过热':
+					row[i]='x1cdf1r'
+				
+				#本场比赛必发成交量倾向于客胜与百家欧赔概率相差较大，谨防客胜过热
+				if row[i]=='本场比赛必发成交量倾向于客胜与百家欧赔概率相差不大':
+					row[i]='x0cbdf'
+				if row[i]=='本场比赛必发成交量倾向于客胜与百家欧赔概率相差较大，谨防客胜过热':
+					row[i]='x0cdf1r'
+
+
+				if row[i]=='本场比赛必发交易规模适中':
+					row[i]='规模适中'
+				#本场比赛必发交易规模较小，数据参考性不强
+				if row[i]=='本场比赛必发交易规模较小，数据参考性不强':
+					row[i]='规模较小'
+				#本场比赛必发交易规模超千万，谨防比赛过于热门
+				if row[i]=='本场比赛必发交易规模超千万，谨防比赛过于热门':
+					row[i]='规模超千万'
+
+				if row[i]=='暂无数据':
+					row[i]=0
+			#print(row[-2])
 		df=DataFrame(vvv,columns=ccc)		
-		#print(df.head())
-		
-		#files='e:/football/{}2.csv'.format(cp.replace('/','-'))
+
 		files1='e:/football/ai2.csv'
 		#print(files)
 		df.to_csv(files1)
@@ -80,8 +117,9 @@ class zq_aicl:
 		#files1='e:/football/{}2.csv'.format(cp)
 		gg=gu_getfromdb()
 		data=gg.get_fromfiles(files1)
+
 		x=data.iloc[:,6:]
-		y=data.iloc[:,3]
+		y=data.iloc[:,5]
 		feature=data.iloc[:,6:].columns
 
 		return x,y,feature
@@ -91,27 +129,32 @@ class zq_aicl:
 		#特征工程
 		x_1=pd.get_dummies(x)
 		#x_2=MinMaxScaler().fit_transform(x_1)
+	
 		x_2=StandardScaler().fit_transform(x_1)
-
-		#print(x_2)
+		
 		#拆分训练集
-		X_train,X_test,y_train,y_test= train_test_split(x_2,y,test_size=0.3, random_state=0)	
+		X_train,X_test,y_train,y_test= train_test_split(x_2,y,test_size=0.35, random_state=0)	
 		return 	X_train,X_test,y_train,y_test,feature
 	#梯度提升机参数调优
 	def ai_td_tc(self,x,y,feature):
 		#导入数据集
 		X_train,X_test,y_train,y_test,feature=self.ai_tzgc(x,y,feature)
 
-		param_test1 = {'n_estimators':range(20,81,10)}
-		param_test2={'max_depth':range(5,16,2)}
-		param_test3 = {'min_samples_split':range(1000,2100,200), 'min_samples_leaf':range(30,71,10)}
-		param_test4 = {'max_features':range(7,20,2)}
+		param_test1 = {'n_estimators':range(10,81,10)}
+		param_test2={'max_depth':range(5,15,2)}
+		param_test3 = {'min_samples_split':range(200,1000,200), 'min_samples_leaf':range(40,61,10)}
+		param_test4 = {'max_features':range(7,10,1)}
 
-		clf=GridSearchCV(estimator=GradientBoostingClassifier(learning_rate=0.01,max_depth=5,max_features=13, n_estimators=20,random_state=0),
-			param_grid = param_test3, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+		clf=GridSearchCV(estimator=GradientBoostingClassifier(max_features=9,max_depth=7,n_estimators=10,learning_rate=0.01,random_state=0,min_samples_leaf=50,min_samples_split=600),
+			param_grid = param_test4, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+		#{'min_samples_leaf': 30, 'min_samples_split': 1600} 0.5457417355371901
+		#{'n_estimators': 30} 0.5384028925619834
+		#{'max_depth': 5} 0.5340371900826447
+		#{'max_features': 17} 0.5527954545454545
 		clf.fit(X_train,y_train)
 		#查看性能#grid_scores_
-		print(clf.cv_results_,clf.best_params_,clf.best_score_)
+		#print(clf.cv_results_,clf.best_params_,clf.best_score_)
+		print(clf.best_params_,clf.best_score_)
 		return 0
 	#梯度提升机成模型
 	def ai_td_xl(self,x,y,feature):
@@ -119,8 +162,7 @@ class zq_aicl:
 	
 		X_train,X_test,y_train,y_test,feature=self.ai_tzgc(x,y,feature)
 
-		clf2=GradientBoostingClassifier(learning_rate=0.03, n_estimators=60,max_depth=5,max_features=13, 
-			subsample=0.8,random_state=0,min_samples_leaf=30,min_samples_split=1000)
+		clf2=GradientBoostingClassifier(learning_rate=0.01, n_estimators=40,max_depth=7,max_features=9, subsample=0.8, random_state=0,min_samples_leaf=60,min_samples_split=400)
 		#clf2=GradientBoostingClassifier(learning_rate=0.01, n_estimators=20,max_depth=6,max_features=13, subsample=0.8, random_state=0,min_samples_leaf=30,min_samples_split=1000)
 
 		clf2.fit(X_train,y_train)
@@ -132,9 +174,68 @@ class zq_aicl:
 
 		joblib.dump(clf2,fil_mx)
 		return 0
+	#逻辑模型
+	def ai_td_xl2(self,x,y,feature):
+		X_train,X_test,y_train,y_test,feature=self.ai_tzgc(x,y,feature)
 
+		pipeline = Pipeline([('poly', PolynomialFeatures(degree=2)),
+			('clf', LogisticRegression())])  
+		parameters = {
+		#'vect__max_df': (0.25, 0.5, 0.75),  
+		#'vect__stop_words': ('english', None),  
+		#'vect__max_features': (2500, 5000, 10000, None),  
+		#'vect__ngram_range': ((1, 1), (1, 2)),  
+		#'vect__use_idf': (True, False),  
+		#'vect__norm': ('l1', 'l2'),  
+		'clf__penalty': ('l1', 'l2'),  
+		'clf__C': (0.01, 0.1, 1, 10),  
+		}
+		grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, scoring='accuracy', cv=3)
+		grid_search.fit(X_train,y_train)
+
+		#predictions = grid_search.predict(X_test)  
+		#print(grid_search.best_params_,grid_search.best_score_)
+		print('最佳效果：%0.3f' % grid_search.best_score_)  
+		print('最优参数组合：')  
+		best_parameters = grid_search.best_estimator_.get_params()  
+		for param_name in sorted(parameters.keys()):  
+			print('\t%s: %r' % (param_name, best_parameters[param_name]))  
+		predictions = grid_search.predict(X_test)  
+		print('准确率：', accuracy_score(y_test, predictions))  
+		print('精确率：', precision_score(y_test, predictions))  
+		print('召回率：', recall_score(y_test, predictions)) 
+
+		#for i,predictions in enumerate(predictions[-5:]):  
+			#print ('预测类型：%s. 信息: %s' %(predictions,y_test[i])) 
+		#scores = cross_val_score(classifer,X_train,y_train,cv=5)
+		clf2 = LogisticRegression(penalty='l1',C=1)
+		clf2.fit(X_train,y_train) 
+		print(" Accuracy on training set: {:.3f}". format( clf2.score( X_train, y_train)))
+		print(" Accuracy on test set: {:.3f}". format( clf2.score( X_test, y_test)))
+		#保存模型
+		dirs='e:/football/'
+		fil_mx=dirs+'1_05_mx.pkl'
+		joblib.dump(clf2,fil_mx)
+		return 0
 	#根据梯度提升机数据预测
+	#三分类线性
+	def linearsvc(self,x,y,feature):
+		X_train,X_test,y_train,y_test,feature=self.ai_tzgc(x,y,feature)
 
+		clf2=LinearSVC()
+		clf2.fit(X_train,y_train)
+		print(clf2.coef_.shape)
+		print(" Accuracy on training set: {:.3f}". format( clf2.score( X_train, y_train)))
+		print(" Accuracy on test set: {:.3f}". format( clf2.score( X_test, y_test)))
+
+		#mglearn.plots.plot_2d_classification(clf2,,fill=True,alpha=0.7)
+		mglearn.discrete_scatter(X_train[:,3],X_train[:,4],y_train)
+		line=np.linspace(-15,15)
+		for coef,intercept,color in zip(clf2.coef_,clf2.intercept_,['b','r','g']):
+			plt.plot(line,-(line*coef[0]+intercept)/coef[1],c=color)
+			plt.legend(['class 0','class 1','class 2'],['L0','L1','L2'],loc=(1.01,0.3))
+		plt.show()
+		return 0
 	def ai_td_yc(self,list1):
 		dirs='e:/football/'
 		fil_mx=dirs+'1_05_mx.pkl'
@@ -149,7 +250,7 @@ class zq_aicl:
 		return y
 
 	def jl(self,x,y,feature):
-		print(feature)
+		#print(feature)
 		#聚类算法
 		x_1=pd.get_dummies(x)
 		x_2=StandardScaler().fit_transform(x_1)
@@ -187,7 +288,7 @@ def main():
 	zz=zq_aicl()
 	zs=zq_sjcl()
 	#zs.jg()
-	#数据处理
+	#1\数据处理
 	print('数据处理')
 	files1=zs.sjcl()
 	
@@ -197,16 +298,18 @@ def main():
 	files1='e:/football/ai2.csv'
 	print('提取数据'+files1)
 	x,y,feature=zz.getfiles(files1)
-	#print(len(x),len(y))
+	print(len(x),len(y))
 	# 聚类
 	#zz.jl(x,y,feature)
-	#特征工程
-	#X_train,X_test,y_train,y_test,feature=zz.ai_tzgc(x,y,feature)
+
 	#调参
 	print('调参')
 	#zz.ai_td_tc(x,y,feature)
 	#训练
-	zz.ai_td_xl(x,y,feature)
+	print('训练')
+	#zz.ai_td_xl(x,y,feature)
+	#zz.ai_td_xl2(x,y,feature)
+	zz.linearsvc(x,y,feature)
 	return 0
 def test2():
 	zz=zq_aicl()
@@ -218,19 +321,23 @@ def test2():
 	ln=len(X_test)
 	#print(X_test)
 
-
 	dirs='e:/football/'
 	fil_mx=dirs+'1_05_mx.pkl'
 	#提取已训练好的模型
 	clk=joblib.load(fil_mx)
-
+	ii=0
+	ii2=0
 	for i in range(0,ln):
 		jg=y_test.values[i]
 		#print(X_test.values.tolist()[i])
 		ycz=clk.predict([X_test[i]])
-		print('结果:{} 预测:{}'.format(jg,ycz))
-		if i>500:break
-	
+		if jg==ycz[0] :
+			ii+=1
+			if jg==1:
+				ii2+=1
+		print('结果:{} 预测:{}'.format(jg,ycz[0]))
+		if i>50:break
+	print(ii,ii2)
 	return 0
 
 def test1():
@@ -296,9 +403,19 @@ def test1():
 	#print(" Accuracy on test set: {:.3f}". format( clf.score( X_test, y_test)))
 	return 0
 
+
+def test3():
+	files1='e:/football/ai_zq_sj.csv'
+	gg=gu_getfromdb()
+	data=gg.get_fromfiles(files1)
+	df=data[data.sg==1][['idnm','zd','kd','sg','cp','jp']]
+	print(len(df))
+
+
+
 if __name__ == '__main__':
 	#h=zqfenxi(0).creat_mxk('半球')
 	#ycl=zq_sjcl().sjcl()
-	#test2()
+	#test3()
 	main()
 	#test2()
